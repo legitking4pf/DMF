13258e589743c9a4c066d8a5348872a7414dbd75
legitking4pf <ayewumijustice123@gmail.com>
2026-06-17 19:26:25 -0700

    chore: purge duplicate JS/TS files to resolve compiler conflicts

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,8 @@ EventEmitter.defaultMaxListeners = 25;
 const stsHeader = `max-age=31536000; includeSubDomains; preload`;
 
 // Content Security Policy
-const cspHeader = `default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://*.public.blob.vercel-storage.com https://www.transparenttextures.com https://images.unsplash.com https://invatlan.hn https://cdn.prod.website-files.com https://www.bancatlan.hn; media-src 'self' blob: data: https://*.public.blob.vercel-storage.com; connect-src 'self' https://*.public.blob.vercel-storage.com;
-  object-src 'self'; frame-ancestors 'none'; 'https://hv4w1qmfjrk8zaij.public.blob.vercel-storage.com;`;
-https://hv4w1qmfjrk8zaij.public.blob.vercel-storage.com
+const cspHeader = `default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://*.public.blob.vercel-storage.com https://www.transparenttextures.com https://images.unsplash.com https://invatlan.hn https://cdn.prod.website-files.com https://www.bancatlan.hn; media-src 'self' blob: data: https://*.public.blob.vercel-storage.com; connect-src 'self' https://*.public.blob.vercel-storage.com; object-src 'self'; frame-ancestors 'none';`;
+
 const referrerPolicy = 'no-referrer';
 const permissionsPolicy = "geolocation=(), camera=(), microphone=(), payment=(), fullscreen=(self)";
 
